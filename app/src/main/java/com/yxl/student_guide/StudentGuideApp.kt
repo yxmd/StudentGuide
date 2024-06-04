e2a5d9756eb078d58834fc1dfec6ba09c8c52d17
yxl <perevalov.aleksey30@gmail.com>
2024-06-04 23:08:25 +0300

    remove info

--- app/src/main/java/com/yxl/student_guide/StudentGuideApp.kt
+++ app/src/main/java/com/yxl/student_guide/StudentGuideApp.kt
@@ -8,6 +8,6 @@ import dagger.hilt.android.HiltAndroidApp
 class StudentGuideApp: Application(){
     override fun onCreate() {
         super.onCreate()
-        MapKitFactory.setApiKey("3b316f43-0a57-41a8-bf69-2c5d12cd9bbf")
+        MapKitFactory.setApiKey("")
     }
 }
--- app/src/main/java/com/yxl/student_guide/core/data/Api.kt
+++ app/src/main/java/com/yxl/student_guide/core/data/Api.kt
@@ -19,6 +19,6 @@ interface Api {
     suspend fun getCollegeInfo(@Path("id") id: Int): Response<Institute>
 
     companion object {
-        const val BASE_URL = "http://192.168.100.2:8080"
+        const val BASE_URL = ""
     }
 }
